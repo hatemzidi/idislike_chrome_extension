12d92d8ce39b75d857793d95af21be06a6af88f1
hatem.zidi <hatem.zidi@gmail.com>
2015-11-13 17:14:09 +0100

    create export folder if doesn't exist

--- build/make.sh
+++ build/make.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+[ -d ../export ] || mkdir ../export
 
 zip -r ../export/idislike-`date +%Y%M%d-%H%m`  ../fonts/* ../icons/* ../images/* ../js/* ../style/* ../background.html ../manifest.json -x "*.DS_Store"
 
